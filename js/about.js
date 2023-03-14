35176099e23c2a1a237836dbe733aaf90e908bda
RRrush37 <50136849+RRrush37@users.noreply.github.com>
2023-03-14 15:56:45 +0800

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,4 +1,4 @@
 $(function () {
-  $(".nav").load("https://raw.githubusercontent.com/RRrush37/personalProject/main/nav.html");
+  $(".nav").load("https://github.com/RRrush37/personalProject/blob/edf853e46a80266aff30e83d34de5d8f8a9e446f/nav.html");
   $(".footer").load("../footer.html");
 });
