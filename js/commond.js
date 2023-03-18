3d23a0e7f23e269ae62424763b815af09319341e
RRrush37 <50136849+RRrush37@users.noreply.github.com>
2023-03-18 09:20:22 +0800

    Update commond.js

--- js/commond.js
+++ js/commond.js
@@ -1,6 +1,4 @@
 $(function () {
   $(".nav").load("../personalProject/nav.html");
   $(".footer").load("../personalProject/footer.html");
-  $(".footer").load("../personalProject/footer.html");
-  $(".footer").load("../personalProject/footer.html");
 });
