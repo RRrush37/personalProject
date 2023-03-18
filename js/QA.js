65b989cc8cde923dbb489c80aa93bc81a3ee067a
RRrush37 <riverstorm1105@gmail.com>
2023-03-18 08:50:16 +0800

    0318_01

--- js/QA.js
+++ js/QA.js
@@ -1,9 +1,9 @@
 $(function () {
   $("#nav").load("../nav.html");
   $(".footer").load("../footer.html");
-  $(".triangle").click(function () {
+  $(".QA ul").click(function () {
     // this.nextElementSibling.style.height = "auto";
-    $(this.nextElementSibling.nextElementSibling).toggleClass("autoHeight");
-    $(this).toggleClass("rotate");
+    $(this).children(".answer").toggleClass("autoHeight");
+    $(this).children(".triangle").toggleClass("rotate");
   });
 });
