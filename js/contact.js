9f6d20e4dbb51fa479b794aad06c3550d0e12ec5
RRrush37 <riverstorm1105@gmail.com>
2023-04-11 14:55:40 +0800

    4/11

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,143 @@
-$(document).ready(
-  $(function () {
-    $(".nav").load("../nav.html");
-    $(".footer").load("../footer.html");
-  })
-);
+$(function () {
+  // alert("React:" + React);
+  let obj = {
+    level: 3,
+    name: "Richard",
+    height: 175,
+  };
+
+  // let root = ReactDOM.createRoot(
+  //   document.getElementsByClassName("flexItem")[0]
+  // );
+  // root.render(<Form />);
+  // let root2 = ReactDOM.createRoot(document.body);
+  // root2.render(React.createElement(font, { level: 3 }));
+  // ReactDOM.render(React.createElement(font, { level: 3 }), document.body);
+});
+
+class Font extends React.Component {
+  render() {
+    return (
+      <h1 className="font" style={{ color: this.props.level }}>
+        hello world {this.props.level}
+      </h1>
+    );
+    // return React.createElement(
+    //   "h" + this.props.level,
+    //   null,
+    //   this.props.content
+    // );
+  }
+}
+
+class fontList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { maxLine: 0, color: ["blue", "red", "green"] };
+    for (let i = 0; i < 10; i++) {
+      window.setTimeout(() => {
+        this.setState((currentState, currentProp) => ({
+          maxLine: currentState.maxLine + 1,
+        }));
+      }, i * 1000 + 2000);
+    }
+  }
+  // componentWillMount() {
+  //   alert("componentWillMount");
+  // }
+  // componentDidMount() {
+  //   alert("componentDidMount");
+  // }
+  // componentWillUpdate() {
+  //   alert("componentWillUpdate");
+  // }
+  // componentDidUpdate() {
+  //   alert("componentDidUpdate");
+  // }
+  render() {
+    let fontArr = [];
+    for (let i = 1; i < this.state.maxLine; i++) {
+      let letter = this.state.color[Math.floor(Math.random() * 3)];
+      fontArr.push(
+        // React.createElement(font, {
+        //   level: i,
+        //   content: letter,
+        // })
+        <Font level={letter}></Font>
+      );
+    }
+    // return React.createElement("div", null, fontArr);
+    return <div>{fontArr}</div>;
+  }
+}
+{
+  /* <form action="" id="form">
+          <p>姓名：</p>
+          <input id="name" name="name" type="text" />
+          <p>email：</p>
+          <input id="email" name="email" type="text" />
+          <p>性別：</p>
+          <input type="radio" name="gender" id="gender" value="男" />
+          <span>男</span>
+          <input type="radio" name="gender" id="gender" value="女" />
+          <span>女</span>
+          <p>您的回饋：</p>
+          <textarea
+            type="textarea"
+            name="feedback"
+            id="feedback"
+            rows="5"
+            cols="33"
+          ></textarea>
+
+          <input value="Submit" type="textarea" onclick="submitform()" />
+        </form> */
+}
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <input
+        value={this.state.value}
+        name={this.props.name}
+        type={this.props.type}
+        id={this.props.id}
+      ></input>
+    );
+  }
+}
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <form action="" id="form">
+        <p>姓名：</p>
+        <Input id="name" name="name" type="text" />
+        <p>email：</p>
+        <Input id="email" name="email" type="text" />
+        <p>性別：</p>
+        <Input type="radio" name="gender" id="gender" value="男" />
+        <span>男</span>
+        <Input type="radio" name="gender" id="gender" value="女" />
+        <span>女</span>
+        <p>您的回饋：</p>
+        <textarea
+          type="textarea"
+          name="feedback"
+          id="feedback"
+          rows="5"
+          cols="33"
+        ></textarea>
+
+        <input value="Submit" type="textarea" onclick="submitform()" />
+      </form>
+    );
+  }
+}
 
 function submitform() {
   let dataArray = $("#form").serializeArray();
--- js/rent.js
+++ js/rent.js
@@ -14,4 +14,35 @@ $(function () {
     // rentMenuItem.innerHTML = window.pageYOffset;
     rentMenu[0].style.top = `calc(${window.pageYOffset}px + 40vh`;
   });
+
+  let main = document.querySelectorAll(".rentItem");
+  let mouseTop, mouseLeft, eleTop, eleLeft, nowTop, nowLeft;
+  for (let i = 0; i < main.length; i++) {
+    main[i].addEventListener("mousedown", function (e) {
+      mouseTop = e.pageY;
+      mouseLeft = e.pageX;
+      eleTop = this.getBoundingClientRect().top;
+      eleLeft = this.getBoundingClientRect().left;
+      nowTop = Number(this.style.top.split("p")[0]);
+      nowLeft = Number(this.style.left.split("p")[0]);
+    });
+    main[i].addEventListener("drag", function (e) {
+      console.log("X : " + e.pageX);
+      console.log("Y : " + e.pageY);
+      console.log("window.scrollY : " + window.scrollY);
+      if (
+        (e.pageX > window.scrollX + 5 || e.pageX < window.scrollX - 5) &&
+        (e.pageY > window.scrollY + 5 || e.pageY < window.scrollY - 5)
+      ) {
+        console.log("X : " + e.pageX);
+        console.log("Y : " + e.pageY);
+        console.log("window.scrollY : " + window.scrollY);
+        console.log("window.scrollX : " + window.scrollX);
+        let pageY = e.clientY || e.pageY;
+        let pageX = e.clientX || e.pageX;
+        this.style.top = window.scrollY + nowTop + pageY - mouseTop + "px";
+        this.style.left = window.scrollX + nowLeft + pageX - mouseLeft + "px";
+      }
+    });
+  }
 });
